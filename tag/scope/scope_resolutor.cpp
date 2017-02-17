0e38f9eb187df17cb4997b4411d5ab9f7e0d050a
monpeco <monpeco@gmail.com>
2017-02-17 12:06:35 +0000

    Update scope resoution, doesn't work with local (outer) and local (inner) variables

--- tag/scope/scope_resolutor.cpp
+++ tag/scope/scope_resolutor.cpp
@@ -2,20 +2,32 @@
 #include<cstdio>
 using namespace std;
 
-int a = 2; // declaración de una variable global a
+int a = 2;              // global variable
 
 int main()
 {
     
     printf("a = %d\n", a);
-    int a = 10;
+    int a = 10;                         //outermost local variable
     printf("a = %d\n", a);
     printf("::a = %d\n", ::a);
-    
+
+    printf("Loop\n");
+    for(int i=0; i<2; i++){
+        int a = 20;                     //inner local variable
+        printf("----a = %d\n", a);
+        printf("----::a = %d\n", ::a);
+    }
+
     /*output
         a = 2
         a = 10
         ::a = 2
+        Loop
+        ----a = 20
+        ----::a = 2
+        ----a = 20
+        ----::a = 2
     */
     
     cout << ":end:" <<endl;
