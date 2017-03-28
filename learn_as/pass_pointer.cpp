e83b21d61639ca8f733639676ceb0579ac6aa935
monpeco <monpeco@gmail.com>
2017-03-28 16:37:29 +0000

    Add example pass pointer as parameter

---
+++ learn_as/pass_pointer.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int i = 1, j = 2;
+void permutar(int *a, int *b);
+
+int main(void)
+{
+    printf("\ni = %d, j = %d", i, j);
+    permutar(&i, &j);
+    printf("\ni = %d, j = %d\n", i, j);
+}
+
+void permutar(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
