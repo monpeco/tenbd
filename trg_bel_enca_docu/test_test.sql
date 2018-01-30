a1b679acb3b352a94bf7ec9c7b70735f08053f39
monpeco <monpeco@gmail.com>
2018-01-30 15:21:02 +0000

    Add 'validacion cod_error' part

--- trg_bel_enca_docu/test_test.sql
+++ trg_bel_enca_docu/test_test.sql
@@ -42,10 +42,90 @@ AS
 	@p_aux_esta_defi varchar(3)
 BEGIN
 	begin
+		print '1 - entrando a test_Test'
+		  begin
+			 set  @hay_err = '0'
+			 set  @hay_era = '0'
+			 set  @mensaje2 =' '
+			 set @codi_empr = 1
+			 set @tipo_docu = 39
+			 set @foli_docu = 101
+		  end
 
-		print 'entrando a test_Test'
+	print '@codi_empr: ' + @codi_empr + ', @tipo_docu: ' + @tipo_docu  + ', @foli_docu: ' + @foli_docu 
 
+	print 'execute PARA_GET_VAL EGATE_TIPO_VALI, @p_salida output, @p_existe output, @p_mensaje output'
+	execute PARA_GET_VAL 'EGATE_TIPO_VALI', @p_salida output, @p_existe output, @p_mensaje output
+	
+	print '@p_salida: ' + @p_salida + ', @p_existe: ' + @p_existe + ', @p_mensaje: ' + @p_mensaje
+
+
+	execute @res = 	dte_chec_rang_foli @codi_empr,@tipo_docu,
+					@corr_rafo output, @foli_docu, @ult_foli output, 
+					@cod_error output, @mensaje output 
+	--+  ', @ult_foli: ' + @ult_foli + ', @cod_error: ' + @cod_error
+	print '@corr_rafo: [' + @corr_rafo + ']'
+	print '@foli_docu: [' + @foli_docu + ']'
+	print '@ult_foli: [' + @ult_foli + ']'
+	print '@cod_error: [' + @cod_error + ']'
+	print '@mensaje: [' + @mensaje + ']'
 	end
+ 
+ --- 2
+ 
+	print '2 - validacion cod_error'
+ 	if @cod_error = 'S' 
+		begin 
+			set @mensaje2 =@mensaje2+ ' -20006 - FOLI_DOCU -'+ @mensaje
+			print '---> @mensaje2: [' + @mensaje2 + ']' --TODO QUITAR
+			
+			if  @p_salida = 'TOTAL' 
+				begin
+					print '---> @p_salida if : [' + @p_salida + ']' --TODO QUITAR
+					set  @hay_err = '1'
+					raiserror (@mensaje2,16,1)
+				end
+			else 
+				if  @p_salida  = 'PARCIAL'
+					begin
+						print '---> @p_salida else : [' + @p_salida + ']' --TODO QUITAR
+						set @p_codi ='20001'
+						execute @res = 	dte_chec_erro  @p_codi,
+										@p_desc_erro output, @p_tipo_erro output,
+										@p_indi_dnte output, @p_existe output, @p_mensaje output
+						
+						print '---> @res: [' + @res + ']' --TODO QUITAR
+						print '---> @p_desc_erro: [' + @p_desc_erro + ']' --TODO QUITAR
+						print '---> @p_tipo_erro: [' + @p_tipo_erro + ']' --TODO QUITAR
+						print '---> @p_indi_dnte: [' + @p_indi_dnte + ']' --TODO QUITAR
+						print '---> @p_existe: ['    + @p_existe + ']' --TODO QUITAR
+						print '---> @p_mensaje: ['   + @p_mensaje + ']' --TODO QUITAR
+						
+						print '------------------------]' --TODO QUITAR
+						print '---> @@p_tipo_dnte: ['   + @p_tipo_dnte + ']' --TODO QUITAR
+						print '---> @@p_tipo_dnte: ['   + @p_tipo_dnte + ']' --TODO QUITAR
+						print '---> @@p_indi_dnte: ['   + @p_indi_dnte + ']' --TODO QUITAR
+						
+						if (@p_tipo_dnte = 'N') OR (@p_tipo_dnte = 'S' AND @p_indi_dnte = 'S')
+							begin
+								print '---> @p_tipo_dnte = N: ' --TODO QUITAR
+								if @p_tipo_erro ='ERR'   
+									begin
+										set  @hay_err = '1'
+										print '---> @hay_err = 1' --TODO QUITAR
+										raiserror (@mensaje2,16,1)
+									end
+								else
+									begin
+										set  @hay_era = '1'
+										print '---> @hay_era = 1' --TODO QUITAR
+									end
+							end 
+					end
+		end
+ 
+ 
+ ---
  
 END
 
